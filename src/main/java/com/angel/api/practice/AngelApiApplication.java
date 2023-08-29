b96148ac35a9335872f18347ed9a6706e1bee598
bgdit <bgdit@d5.bhavishgraphics.com>
2023-08-29 19:23:50 +0530

    angel api create.

---
+++ src/main/java/com/angel/api/practice/AngelApiApplication.java
@@ -0,0 +1,13 @@
+package com.angel.api.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/api/practice/AngelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.api.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
